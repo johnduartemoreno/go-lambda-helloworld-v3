f6fc1b41bd3147f178bb90bc7b4f6444f556bdb6
John Alexander Duarte <arquitecto.informacion@ada.co>
2024-05-09 20:59:20 -0500

    Deploy

--- lib/go-lambda-helloworld-v3-stack.ts
+++ lib/go-lambda-helloworld-v3-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class GoLambdaHelloworldV3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define la función Lambda
+    const helloLambda = new lambda.Function(this, 'HelloLambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('.'), // Esto especifica que el código de la función Lambda se encuentra en el directorio raíz del proyecto
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GoLambdaHelloworldV3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Crea un endpoint de API Gateway que invoque la función Lambda cuando se haga una solicitud GET
+    const api = new apigw.LambdaRestApi(this, 'HelloApi', {
+      handler: helloLambda,
+      proxy: true,
+    });
+
+    // Agrega un recurso raíz al endpoint de API Gateway
+    const apiRoot = api.root;
+
+    // Agrega un método GET al recurso raíz
+    apiRoot.addMethod('GET');
   }
 }
+
